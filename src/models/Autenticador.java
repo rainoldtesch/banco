de37653605899f63ef2edf1781d5503debe1bb4f
Rainold Tesch <rainold.tesch@gmail.com>
2023-02-28 10:21:27 -0300

    Criação de interface funcional para autenticar senha - Autenticador

--- src/models/Autenticador.java
+++ src/models/Autenticador.java
@@ -1,8 +1,6 @@
 package models;
 
 @FunctionalInterface
-
 public interface Autenticador {
-    public abstract boolean autentica (String senha) {
-
+    public abstract boolean autentica (String senha);
 }
